5afd4e7bbf361d15db8da20e36cfc2d7dce75076
Szymon Slowik <szymon@slowik.org.pl>
2024-07-18 21:11:47 +0200

    replace / /_/,/---/--

--- space-replacement.sh
+++ space-replacement.sh
@@ -1,6 +1,26 @@
 #!/bin/bash
 
-directory="~/Downloads";
+directory="$HOME/syncthing/H/Z_H-Videos";
 
 \ls $directory > file_list;
 
+i=0;
+
+rm file_list_new;
+
+while read line
+do
+        
+        new=${line// /-};
+        new=${new//_/-};
+        new=${new//,/-};
+
+        new=${new//---/-};
+        new=${new//--/-};
+        new=${new//-./-};
+
+        echo -e "$line\n$new" >> file_list_new;
+
+done < file_list
+
+rm file_list;
